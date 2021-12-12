79be312d66e7999589db2811e7785341fde5c7be
Gercogor <gercogor@gmail.com>
2021-12-12 23:05:44 +0700

    header
    header

--- my-app/src/components/header/headerUp/headerRight/headerRight.js
+++ my-app/src/components/header/headerUp/headerRight/headerRight.js
@@ -1,10 +1,25 @@
-import "./headerRight.css"
+import "./headerRight.css";
+import basketImg from "./basket.png";
+import likeImg from "./like.png";
+import profileImg from "./profile.png";
 
-const HeaderRight = ()=>{
+const HeaderRight = () => {
     return (
         <div className="header-right">
+            <p>37 789  rub</p>
+            <a href="basket">
+                <img src={basketImg} alt="basket" />
+            </a>
+            <a href="like">
+                <img src={likeImg} alt="Like" />
+            </a>
 
-        </div>
+            <a href="profile">
+                <img src={profileImg} alt="Like" />
+            </a>
+
+
+        </div >
     )
 }
 
